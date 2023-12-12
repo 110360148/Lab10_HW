54cd9c9a2a2097f21d282e5b054a87cef68fae29
110360148 <t110360148@ntut.edu.tw>
2023-12-12 16:35:01 +0800

    Lab10

---
+++ Lab10_HW_Service/app/src/main/java/com/example/lab10_hw_service/MyService.kt
@@ -0,0 +1,39 @@
+package com.example.lab10_hw_service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(5000)
+            val intent = Intent(this@MyService, MainActivity2::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            this@MyService.startActivity(intent)
+            stopSelf()
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        CoroutineScope(Dispatchers.IO).cancel()
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+}
